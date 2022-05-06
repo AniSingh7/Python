54bc25fc827fb680149661ba49b65116c494f37f
anisha <anisinghp7@gmail.com>
2022-05-06 15:32:51 +0530

    varibles_class

---
+++ variables.py
@@ -0,0 +1,60 @@
+"""print("Hello world")
+
+if (5>2):
+    print("5 is greater ")
+
+x=5
+x="Sally"
+y="Hello India"
+print(x)
+print(y)
+print(type(x),type(y))
+#print("Hello")
+
+
+print(z)
+a='4'
+b=str(7)
+print(type(b),b)
+print(type(a),a)
+cc=99
+c=int(9)
+print(cc)
+print(type(c),c)
+ccc=float(10)
+print(ccc)
+"""
+x="orange"
+y="apple"
+z="kiwi"
+print(x,y,z)
+a=b=c="pomogranate"
+print(  "\n" ,a,"\n", b, "\n",c)
+
+fruits=["apple","mango","dragon"]
+x,y,z=fruits
+print(x)
+print(y)
+print(z)
+
+#casting
+print("implicit casting")
+x=10
+y=float(x)
+print(type(x),x,type(y),y)
+
+print("explicit casting")
+x=20.5
+y=int(x)
+print(type(x),x,type(y),y)
+
+print("list:")
+x=[1,2,10.5,True,"anisha"]
+print(type(x), id(x),type(x[2]) )
+
+print("unpacking")
+fruits = ["apple", "banana", "grapes", "dragon"]
+x,y,z,k=fruits
+print( x,"\n",y,"\n",z,"\n",k)
+
+
