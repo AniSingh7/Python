58002a54989a847ce0e240b48856b7a70fc6e2fc
anisha <anisinghp7@gmail.com>
2022-05-07 16:16:13 +0530

    unpacking nested list

--- VN2_T03/assessment/list.py
+++ VN2_T03/assessment/list.py
@@ -1,7 +1,11 @@
 list1=[1,2,3,[4,5,6]]
 
 nested_list=list1[3]
-x,y,z=nested_list
-print('\n',x,'\n',y,'\n',z)
+X = nested_list
+for i in X:
+    print(i)
+
+#x,y,z=nested_list
+#print('\n',x,'\n',y,'\n',z)
 
 
